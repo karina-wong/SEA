2215afb98fe28fe11a61202d49013cdaf25ed2f2
NathaN158 <106011846@student.swin.edu.au>
2025-05-25 22:29:14 +1000

    Updated about page contribution and enhancements list

--- enhancements.php
+++ enhancements.php
@@ -1,14 +1,12 @@
-<!-- Explain your enhancements here -->
+<!-- Explained enhancements -->
  <!--
-* Provided the manager with the ability to select the field on which to sort the order in
-which the EOI records are displayed.
-
-
 • Created a manager registration page with server side validation requiring unique
 username and a password rule, and store this information in a table.
-DONE
 
 • Controlled access to manage.php by checking username and password.
-DONE -> admin_login.php & checks validation so if someone went to it via code it still wouldn't work
+admin_login.php & checks validation so if someone went to it via code it still wouldn't work
+
+• Created a manage job listings section in manage.php where an admin can add new job listings and delete existing ones, 
+this automatically updates on jobs.php and apply.php
 
 -->
